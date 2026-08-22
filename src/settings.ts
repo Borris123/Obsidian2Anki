ec02a64d28f723d60e45d21f96a8f4a7e2e9e5f2
Erik Mielke <erikmielke.2@gmail.com>
2026-08-22 19:23:43 +0200

    add searchable anki deck selection and creation

--- src/settings.ts
+++ src/settings.ts
@@ -4,21 +4,27 @@ import {
 	Setting,
 } from "obsidian";
 
-import AnkiExporterPlugin from "./main";
+import type AnkiExporterPlugin
+	from "./main";
 
 export interface AnkiExporterSettings {
 	ankiConnectUrl: string;
 }
 
-export const DEFAULT_SETTINGS: AnkiExporterSettings = {
-	ankiConnectUrl: "http://127.0.0.1:8765",
+export const DEFAULT_SETTINGS:
+	AnkiExporterSettings = {
+
+	ankiConnectUrl:
+		"http://127.0.0.1:8765",
 };
 
-export class AnkiExporterSettingTab extends PluginSettingTab {
+export class AnkiExporterSettingTab
+	extends PluginSettingTab {
 
 	constructor(
 		app: App,
-		private readonly plugin: AnkiExporterPlugin,
+		private readonly plugin:
+		AnkiExporterPlugin,
 	) {
 		super(app, plugin);
 	}
@@ -30,16 +36,30 @@ export class AnkiExporterSettingTab extends PluginSettingTab {
 
 		new Setting(containerEl)
 			.setName("AnkiConnect URL")
-			.setDesc("URL used to connect to AnkiConnect.")
+			.setDesc(
+				"URL used to communicate with AnkiConnect.",
+			)
 			.addText(text =>
 				text
-					.setPlaceholder("http://127.0.0.1:8765")
-					.setValue(this.plugin.settings.ankiConnectUrl)
-					.onChange(async value => {
-						this.plugin.settings.ankiConnectUrl = value;
+					.setPlaceholder(
+						"http://127.0.0.1:8765",
+					)
+					.setValue(
+						this.plugin
+							.settings
+							.ankiConnectUrl,
+					)
+					.onChange(
+						async value => {
+							this.plugin
+								.settings
+								.ankiConnectUrl =
+								value.trim();
 
-						await this.plugin.saveSettings();
-					}),
+							await this.plugin
+								.saveSettings();
+						},
+					),
 			);
 	}
 }
